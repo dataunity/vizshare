d46d5af63e89b7c98ecdfa16f0407d34b9fb66c4
Kev K <kev@dataunity.org>
2014-04-29 11:20:25 +0100

    Add jQuery plugin style of renderering.

--- vizshare-0.1.0.js
+++ vizshare-0.1.0.js
@@ -144,3 +144,51 @@ vizshare.dataSetHelper = (function () {
     // return the constructor to be assigned to the new namespace
     return Constr;
 } ());
+
+// jQuery wrapper
+// ToDo: split jquery plug in into a separate file?
+(function ($) {
+    $.fn.duGetPath = function () {
+        // From http://stackoverflow.com/questions/2068272/getting-a-jquery-selector-for-an-element
+        if (this.length != 1) throw 'Requires one element.';
+
+        var path, node = this;
+        while (node.length) {
+            var realNode = node[0];
+            var name = (
+                // IE9 and non-IE
+                realNode.localName ||
+                // IE <= 8
+                realNode.tagName ||
+                realNode.nodeName
+            );
+
+            // on IE8, nodeName is '#document' at the top level, but we don't need that
+            if (!name || name == '#document') break;
+
+            name = name.toLowerCase();
+            if (realNode.id) {
+                // As soon as an id is found, there's no need to specify more.
+                return name + '#' + realNode.id + (path ? '>' + path : '');
+            } else if (realNode.className) {
+                name += '.' + realNode.className.split(/\s+/).join('.');
+            }
+
+            var parent = node.parent(), siblings = parent.children(name);
+            if (siblings.length > 1) name += ':eq(' + siblings.index(node) + ')';
+            path = name + (path ? '>' + path : '');
+
+            node = parent;
+        }
+
+        return path;
+    };
+
+    $.fn.vizshare = function (opts) {
+        return this.each(function () {
+            var path = $(this).duGetPath(),
+                renderOpts = $.extend({}, opts, {selector: path});
+            vizshare.render(renderOpts);
+        });
+    };
+}(jQuery));
